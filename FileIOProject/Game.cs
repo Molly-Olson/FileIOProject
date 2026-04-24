45e2f119fcd313e46a649e86ea4a8856fe770efd
Molly Olson <molly.olson@student.centralia.edu>
2026-04-24 12:20:45 -0700

    Add project files.

---
+++ FileIOProject/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOProject
+{
+    public class Game
+    {
+        public Player? Player { get; set; } // why does it want it non nullable so bad?!
+    }
+}
---
+++ FileIOProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace FileIOProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Temp\PlayerData.json";
+
+            Console.WriteLine("Let's do some JSON file IO saving shall we?");
+            Console.WriteLine("Maybe...");
+            Console.WriteLine();
+            Console.WriteLine(" ----------------------------------------------------- ");
+            Console.WriteLine("Enter Player Name.");
+            Console.WriteLine(" ----------------------------------------------------- ");
+            Console.WriteLine();
+
+            string playerName = Console.ReadLine();
+
+            Player player = new Player { Name = playerName };
+            Game game = new Game { Player = player };
+
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    
+                }
+            }
+            
+        }
+    }
+}
